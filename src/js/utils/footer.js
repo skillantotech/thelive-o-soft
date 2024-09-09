89e8f31ad3c965f5021cab219e11c2bb6c7ced52
unknown <sasmitasubhashree@gmail.com>
2024-09-09 13:28:09 +0530

    Faq Subhashree branch

---
+++ src/js/utils/footer.js
@@ -0,0 +1,10 @@
+fetch("../Components/Footer/footer.html")
+  .then((response) => {
+    console.log(response);
+    return response.text();
+  })
+  .then((data) => {
+    console.log(data)
+    const Footer = document.getElementById("footer");
+    Footer.innerHTML = data;
+  });
