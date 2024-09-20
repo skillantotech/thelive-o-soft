8d79d39ec3bcf77b1151db795c7fc6d48f178bd3
unknown <sasmitasubhashree@gmail.com>
2024-09-20 16:20:22 +0530

    Changes in Navbar & all pages hero section image height&responsive

--- src/js/utils/header.js
+++ src/js/utils/header.js
@@ -6,7 +6,7 @@
 //     document.getElementById("header").innerHTML = data;
 //     // console.log(data);
 //   });
-fetch("../Components/Header/header.html")
+fetch("/src/Components/Header/header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
