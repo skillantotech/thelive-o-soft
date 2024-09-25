d035115b0e30c3df3ae47b0f0f2efac43b4bcc10
manishrout45 <manishkumarrout45@gmail.com>
2024-09-25 22:03:59 +0530

    Index, About added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        }
+      },
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+      }
+    },
+    plugins: [],
+  }
 }
-
