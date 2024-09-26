95383802c882c4c1f16fc235792a0542deefcb43
unknown <sasmitasubhashree@gmail.com>
2024-09-26 23:18:42 +0530

    Changes the css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",],
+  content: ["./index.html", "./src/*/.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
