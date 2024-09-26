fe7be64b72451a3748e19651a21d443be8f91127
manishrout45 <manishkumarrout45@gmail.com>
2024-09-26 14:06:37 +0530

    Index, about, inputcss

--- src/js/utils/header.js
+++ src/js/utils/header.js
@@ -1,13 +1,15 @@
-fetch("../Components/Header/header.html")
-  .then((response) => {
-    return response.text();
-  })
+// fetch("../Components/Header/header.html")
+//   .then((response) => {
+//     return response.text();
+//   })
+//   .then((data) => {
+//     document.getElementById("header").innerHTML = data;
+//     // console.log(data);
+//   });
+fetch("/src/Components/Header/header.html")
+  .then((response) => response.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
-    // console.log(data);
-  });
-
-  document.getElementById('menu-btn').addEventListener('click', function() {
-    const mobileMenu = document.getElementById('mobile-menu');
-    mobileMenu.classList.toggle('hidden');
-});
+    initializeHeader(); // Initialize JavaScript after content is loaded
+  })
+  .catch((error) => console.error("Error loading the header:", error));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}",],
   theme: {
-    extend: {
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-50%)' },
-        }
-      },
-      animation: {
-        marquee: 'marquee 20s linear infinite',
-      }
-    },
-    plugins: [],
-  }
+    extend: {},
+  },
+  plugins: [],
 }
+
