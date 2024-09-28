1d0ae66d374bc2500b91fa8b39821eabe465638e
Milan6299 <milanmahapatra140@gmail.com>
2024-09-28 19:15:14 +0530

    All pages done of scope 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}",],
+  content: ["./index.html","./src/*/**.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
