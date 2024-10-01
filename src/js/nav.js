a2442129172437d7735063ef6bfc962986a7009a
unknown <sasmitasubhashree@gmail.com>
2024-10-01 18:57:20 +0530

    hospitally service &navbar

--- src/js/nav.js
+++ src/js/nav.js
@@ -36,12 +36,12 @@ function initializeDropdown() {
 }
 
 function initializeAccordion() {
+  const menuBtn = document.getElementById("menu-btn");
+  const content = document.getElementById("accordion-content-mobilemenu");
+  const icon = document.getElementById("icon");
+
   function toggleAccordion() {
-    const content = document.getElementById("accordion-content-mobilemenu");
-    const icon = document.getElementById("icon");
-
     content.classList.toggle("hidden");
-
     // Change the icon from angle-down to angle-right and vice versa
     if (content.classList.contains("hidden")) {
       icon.classList.remove("fa-angle-down");
@@ -52,10 +52,29 @@ function initializeAccordion() {
     }
   }
 
-  // Add event listener for the menu button
-  document
-    .getElementById("menu-btn")
-    .addEventListener("click", toggleAccordion);
+  menuBtn.addEventListener("click", toggleAccordion);
+}
+
+function initializeAccordionPlatfrom() {
+  const menuBtnPlatfrom = document.getElementById("menu-btn-platfrom");
+  const contentPlatfrom = document.getElementById(
+    "accordion-content-mobilemenu-platfrom"
+  );
+  const iconPlatfrom = document.getElementById("icon_platfrom");
+
+  function toggleAccordionPlatfrom() {
+    contentPlatfrom.classList.toggle("hidden");
+
+    if (contentPlatfrom.classList.contains("hidden")) {
+      iconPlatfrom.classList.remove("fa-angle-down");
+      iconPlatfrom.classList.add("fa-angle-right");
+    } else {
+      iconPlatfrom.classList.remove("fa-angle-right");
+      iconPlatfrom.classList.add("fa-angle-down");
+    }
+  }
+
+  menuBtnPlatfrom.addEventListener("click", toggleAccordionPlatfrom);
 }
 
 // Function to initialize all components
@@ -63,7 +82,9 @@ function initializeHeader() {
   initializeMobileMenu();
   initializeDropdown();
   initializeAccordion();
+  initializeAccordionPlatfrom();
 }
+
 document.addEventListener("DOMContentLoaded", function () {
   initializeHeader(); // Initialize JavaScript on initial page load
 });
