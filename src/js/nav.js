b7f8495161c3898a02688d94beb8222f7ce29d0b
unknown <sasmitasubhashree@gmail.com>
2024-10-05 20:23:11 +0530

    added faq part

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,40 +3,36 @@ function initializeMobileMenu() {
   const mobileMenu = document.getElementById("mobile-menu");
   const closeMenu = document.getElementById("close-menu");
 
-  if (navbarToggle && mobileMenu && closeMenu) {
-    navbarToggle.addEventListener("click", () => {
-      mobileMenu.classList.toggle("hidden");
-    });
+  navbarToggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
 
-    closeMenu.addEventListener("click", () => {
-      mobileMenu.classList.add("hidden");
-    });
-  }
+  closeMenu.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+  });
 }
 
 function initializeDropdown() {
   const dropdownBtn = document.getElementById("dropdownBtn");
   const dropdownContent = document.getElementById("dropdownContent");
 
-  if (dropdownBtn && dropdownContent) {
-    dropdownBtn.addEventListener("mouseover", function () {
-      dropdownContent.classList.remove("hidden");
-    });
+  dropdownBtn.addEventListener("mouseover", function () {
+    dropdownContent.classList.remove("hidden");
+  });
 
-    dropdownBtn.addEventListener("mouseleave", function () {
-      if (!dropdownContent.matches(":hover")) {
-        dropdownContent.classList.add("hidden");
-      }
-    });
-
-    dropdownContent.addEventListener("mouseover", function () {
-      dropdownContent.classList.remove("hidden");
-    });
-
-    dropdownContent.addEventListener("mouseleave", function () {
+  dropdownBtn.addEventListener("mouseleave", function () {
+    if (!dropdownContent.matches(":hover")) {
       dropdownContent.classList.add("hidden");
-    });
-  }
+    }
+  });
+
+  dropdownContent.addEventListener("mouseover", function () {
+    dropdownContent.classList.remove("hidden");
+  });
+
+  dropdownContent.addEventListener("mouseleave", function () {
+    dropdownContent.classList.add("hidden");
+  });
 }
 
 function initializeAccordion() {
@@ -44,21 +40,19 @@ function initializeAccordion() {
   const content = document.getElementById("accordion-content-mobilemenu");
   const icon = document.getElementById("icon");
 
-  if (menuBtn && content && icon) {
-    function toggleAccordion() {
-      content.classList.toggle("hidden");
-
-      if (content.classList.contains("hidden")) {
-        icon.classList.remove("fa-angle-down");
-        icon.classList.add("fa-angle-right");
-      } else {
-        icon.classList.remove("fa-angle-right");
-        icon.classList.add("fa-angle-down");
-      }
+  function toggleAccordion() {
+    content.classList.toggle("hidden");
+    // Change the icon from angle-down to angle-right and vice versa
+    if (content.classList.contains("hidden")) {
+      icon.classList.remove("fa-angle-down");
+      icon.classList.add("fa-angle-right");
+    } else {
+      icon.classList.remove("fa-angle-right");
+      icon.classList.add("fa-angle-down");
     }
-
-    menuBtn.addEventListener("click", toggleAccordion);
   }
+
+  menuBtn.addEventListener("click", toggleAccordion);
 }
 
 function initializeAccordionPlatform() {
@@ -68,21 +62,19 @@ function initializeAccordionPlatform() {
   );
   const iconPlatform = document.getElementById("icon_platform");
 
-  if (menuBtnPlatform && contentPlatform && iconPlatform) {
-    function toggleAccordionPlatform() {
-      contentPlatform.classList.toggle("hidden");
+  function toggleAccordionPlatform() {
+    contentPlatform.classList.toggle("hidden");
 
-      if (contentPlatform.classList.contains("hidden")) {
-        iconPlatform.classList.remove("fa-angle-down");
-        iconPlatform.classList.add("fa-angle-right");
-      } else {
-        iconPlatform.classList.remove("fa-angle-right");
-        iconPlatform.classList.add("fa-angle-down");
-      }
+    if (contentPlatform.classList.contains("hidden")) {
+      iconPlatform.classList.remove("fa-angle-down");
+      iconPlatform.classList.add("fa-angle-right");
+    } else {
+      iconPlatform.classList.remove("fa-angle-right");
+      iconPlatform.classList.add("fa-angle-down");
     }
-
-    menuBtnPlatform.addEventListener("click", toggleAccordionPlatform);
   }
+
+  menuBtnPlatform.addEventListener("click", toggleAccordionPlatform);
 }
 
 // Function to initialize all components
