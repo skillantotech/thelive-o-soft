4e129886efd66b27eefa851046dc848c85af7ec0
unknown <sasmitasubhashree@gmail.com>
2024-10-21 18:16:46 +0530

    changes inhero section

--- src/js/utils/header.js
+++ src/js/utils/header.js
@@ -10,6 +10,15 @@ fetch("/src/Components/Header/header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
+    console.log("initializeHeader is defined, calling it now");
     initializeHeader(); // Initialize JavaScript after content is loaded
   })
   .catch((error) => console.error("Error loading the header:", error));
+// document.addEventListener("DOMContentLoaded", function () {
+//   if (typeof initializeHeader === "function") {
+//     console.log("initializeHeader is defined, calling it now");
+//     initializeHeader();
+//   } else {
+//     console.error("initializeHeader is not defined");
+//   }
+// });
